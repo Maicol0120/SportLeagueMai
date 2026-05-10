25499d0d86c0126848f53b692f45acb3c9f73506
Maicol <Maicol@LAPTOP-P1RAML2V>
2026-05-10 00:18:37 -1200

    Add project files.

---
+++ SportsLeague.API/Mappins/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using SportsLeague.API.DTO_s.Request;
+using SportsLeague.API.DTO_s.Response;
+using SportsLeague.Domain.Entities;
+
+namespace SportsLeague.API.Mappins
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Team mappings
+            CreateMap<TeamRequestDTO, Team>();
+            CreateMap<Team, TeamResponseDTO>();
+        }
+    }
+
+}
