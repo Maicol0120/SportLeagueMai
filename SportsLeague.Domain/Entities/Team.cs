17ccd747bcc631ef86f810149a8721cf752a6f75
MAICOL VILLA <maicolestivilla2003@outlook.es>
2026-05-15 12:06:14 -1200

    add fase #4

--- SportsLeague.Domain/Entities/Team.cs
+++ SportsLeague.Domain/Entities/Team.cs
@@ -8,6 +8,14 @@
         public string Stadium { get; set; } = string.Empty;
         public string? LogoUrl { get; set; }
         public DateTime FoundedDate { get; set; }
+
+        // Navigation Property - Colección de jugadores
+        public ICollection<Player> Players { get; set; } = new List<Player>();
+        public ICollection<TournamentTeam> TournamentTeams { get; set; }
+       = new List<TournamentTeam>();
+
+
+
     }
 
 }
