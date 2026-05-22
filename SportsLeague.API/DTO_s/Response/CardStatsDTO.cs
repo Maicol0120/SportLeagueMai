24bf627ec7d3c3d65894e6810a3fbeb8ba3851ee
MAICOL VILLA <maicolestivilla2003@outlook.es>
2026-05-21 23:14:22 -1200

    actualizate

--- SportsLeague.API/DTO_s/Response/CardStatsDTO.cs
+++ SportsLeague.API/DTO_s/Response/CardStatsDTO.cs
@@ -1,13 +1,16 @@
 ﻿namespace SportsLeague.API.DTO_s.Response
 {
-    public class CardStatsDTO
+    namespace SportsLeague.API.DTO_s.Response
     {
-        public int PlayerId { get; set; }
-        public string PlayerName { get; set; } = string.Empty;
-        public string TeamName { get; set; } = string.Empty;
-        public int YellowCards { get; set; }
-        public int RedCards { get; set; }
-        public int TotalCards { get; set; }
-    }
+        public class CardStatsDTO
+        {
+            public int PlayerId { get; set; }
+            public string PlayerName { get; set; } = string.Empty;
+            public string TeamName { get; set; } = string.Empty;
+            public int YellowCards { get; set; }
+            public int RedCards { get; set; }
+            public int TotalCards { get; set; }
+        }
 
+    }
 }
--- SportsLeague.API/DTO_s/Response/StandingDTO.cs
+++ SportsLeague.API/DTO_s/Response/StandingDTO.cs
@@ -1,18 +1,21 @@
-﻿ namespace SportsLeague.API.DTO_s.Response
+﻿namespace SportsLeague.API.DTO_s.Response
 {
-    public class StandingDTO
+    namespace SportsLeague.API.DTO_s.Response
     {
-        public int Position { get; set; }
-        public int TeamId { get; set; }
-        public string TeamName { get; set; } = string.Empty;
-        public int MatchesPlayed { get; set; }     // PJ
-        public int Wins { get; set; }              // PG
-        public int Draws { get; set; }             // PE
-        public int Losses { get; set; }            // PP
-        public int GoalsFor { get; set; }          // GF
-        public int GoalsAgainst { get; set; }      // GC
-        public int GoalDifference { get; set; }    // DG
-        public int Points { get; set; }            // Pts
-    }
+        public class StandingDTO
+        {
+            public int Position { get; set; }
+            public int TeamId { get; set; }
+            public string TeamName { get; set; } = string.Empty;
+            public int MatchesPlayed { get; set; }     // PJ
+            public int Wins { get; set; }              // PG
+            public int Draws { get; set; }             // PE
+            public int Losses { get; set; }            // PP
+            public int GoalsFor { get; set; }          // GF
+            public int GoalsAgainst { get; set; }      // GC
+            public int GoalDifference { get; set; }    // DG
+            public int Points { get; set; }            // Pts
+        }
 
+    }
 }
